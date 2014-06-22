7d5c243a45c610399c1b3d9347b226bfecfdb997
jfraboni <johnfraboni@hotmail.com>
2014-06-22 00:23:19 -0400

    * update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 (shopt -s dotglob; mv -- * ..)
 echo "Files relocated"
 cd ../ && npm install
-
-#rm $0
-#rm -rf `node-magic-eight-ball-lesson $0`
+cd -
+rm $0
+rm -rf `node-magic-eight-ball-lesson $0`
