44617732617b040426bd98865a9b21203fecdcbb
jfraboni <johnfraboni@hotmail.com>
2014-06-22 00:45:06 -0400

    * update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+if [ -e "../package.json" ]
+then
+  echo "removing original package.json"
+  rm ../package.json
+fi
+if [ -e "../server.js" ]
+then
+  echo "removing original server.js"
+  rm ../server.js
+fi
 
 (shopt -s dotglob; mv -- * ..)
 echo "Files relocated"
