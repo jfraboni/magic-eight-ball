c7653f65464cb7295ca864733db8e656b72dc234
jfraboni <johnfraboni@hotmail.com>
2014-07-04 19:47:49 -0400

    * update install

--- setup.sh
+++ setup.sh
@@ -20,27 +20,27 @@ if [[ `hostname -s` = "runnable" ]]
 then
   echo "...on runnable..."
 
-  if [ -e "../package.json" ]
+  if [ -e "package.json" ]
 	then
 	  echo "removing original package.json"
-	  rm ../package.json
+	  rm package.json
   fi
-  if [ -e "../server.js" ]
+  if [ -e "server.js" ]
 	then
 	  echo "removing original server.js"
-	  rm ../server.js
+	  rm server.js
   fi
+
+  mv spark/* .
+  rm -rf spark
 
   ./replace-anchor.js
   rm replace-anchor.js
 
-  #(shopt -s dotglob; mv "${config[0]}/*" .)
-  mv spark/* .
   echo "Files relocated to root, installing node packages..."
-  cd ../ && npm install
+  npm install
   
   rm setup.sh
-  rm -rf "${config[0]}"
 else
   npm install
   rm setup.sh
